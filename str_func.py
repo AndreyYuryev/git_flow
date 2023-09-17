05c325a8da5505d7df2b790c57f36b3b57d97c43
Andrey <yuryev@yandex.ru>
2023-09-17 21:57:24 +0300

    add upper function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def capitalize_str(in_str):
     ''' return capitalised string '''
     return in_str.capitalize()
+
+
+def upper_str(in_str):
+    ''' return upper string '''
+    return in_str.upper()
